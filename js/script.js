2fad6535d776c2f86c143aacd958f0334e4e1d76
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-05-31 13:22:02 -0600

    final changes

--- js/script.js
+++ js/script.js
@@ -5,17 +5,19 @@ const bio = document.querySelector('.bio-container');
 const resume = document.querySelector('.resume-container');
 const projects = document.querySelector('.projects-container');
 const message = document.querySelector('.message-container');
+const form = document.querySelector('.form-wrapper');
 
 // Constants for buttons
-const navBtn = document.querySelector('.hamburger-icon-link')
-const homeBtn = document.querySelector('#home-link')
+const navBtn = document.querySelector('.hamburger-icon-link');
+const homeBtn = document.querySelector('#home-link');
 const bioBtn = document.querySelector('#bio-link');
 const resumeBtn = document.querySelector('#resume-link');
 const projectsBtn = document.querySelector('#projects-link');
 const messageBtn = document.querySelector('#message-link');
+const submitBtn = document.querySelector('#submit');
 
 // event listeners
-navBtn.addEventListener('click', function(event) {
+navBtn.addEventListener('click', function (event) {
 	event.preventDefault();
 	nav.classList.remove('hide');
 	home.classList.add('hide');
@@ -23,7 +25,7 @@ navBtn.addEventListener('click', function(event) {
 	resume.classList.add('hide');
 	projects.classList.add('hide');
 	message.classList.add('hide');
-})
+});
 homeBtn.addEventListener('click', function (event) {
 	event.preventDefault();
 	home.classList.remove('hide');
@@ -69,5 +71,27 @@ messageBtn.addEventListener('click', function (event) {
 	resume.classList.add('hide');
 	projects.classList.add('hide');
 });
+form.addEventListener('submit', function (event) {
+	event.preventDefault();
+	const formName = document.querySelector('#name-box').value;
+	const formEmail = document.querySelector('#email-box').value;
+	const formMessage = document.querySelector('#message-box').value;
+	names.push(formName);
+	emails.push(formEmail);
+	inbox.push(formMessage);
+	console.log('form submitted');
+});
 
-// Submit form
+let names = [];
+let emails = [];
+let inbox = [];
+
+const gitBtn = document.querySelector('.git-para-a');
+
+const gitPara = document.querySelector('.git-para');
+
+gitBtn.addEventListener('click', function (event) {
+	event.preventDefault();
+	gitPara.classList.remove('hide');
+	gitPara.classList.add('fade');
+});
